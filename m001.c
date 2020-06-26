618deab10d7e2769935a224f04f30af1e58f483c
AllanVieiraCoel <allan_vc@usp.br>
2020-06-26 17:26:19 +0000

    entrega m001.3

--- m001.c
+++ m001.c
@@ -25,19 +25,22 @@
 int greatest (int a, int b, int c)
 {
   if(a>b && a>c)
+  {
+    return a;
+  }
+  else 
+  {
+    if(b>a && b>c)
     {
-      return a;
-       }
-    else {
-        if(b>a && b>c){
-        return b;
-        }else 
-        if(c>a && c>b){
-            return c;
-        };
-    }   
-return 0;
+      return b;
+    }
+     else 
+     { 
+      return c;   
+     }
+  }   
 }
+
 /* Do not edit this function. */
 
 int main (int argc, char **argv)
