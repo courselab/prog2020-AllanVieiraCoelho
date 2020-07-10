9ced9a92728337e80389a19e4677796a066a4939
AllanVieiraCoel <allan_vc@usp.br>
2020-07-10 21:26:10 +0000

    m002 t2

--- m002.c
+++ m002.c
@@ -26,27 +26,28 @@
 /* Sort three integers x, y, z in ascending order.*/
 
 void sort (int *a, int *b, int *c)
-{int k;        
+{
+  int k;        
     
     if (*a>*c)    
     {
-         k = *c;
-        *a = k;
+        k = *c;
         *c = *a;
-        
+        *a = k;
     }
     
-    if (*b>*c)   
-    {
-        k = *c;
-        *c = *b;
-        *b = k;
-    }
     if (*a>*b)   
     {
         k = *b;
         *b = *a;
         *a = k;
+    }
+    
+    if (*b>*c)   
+    {
+       k = *c;
+        *c = *b;
+        *b = k;
     }
 }
 
